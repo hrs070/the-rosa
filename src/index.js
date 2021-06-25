86409ed3ee5285ab8222f0f6b7dad343e6f938b1
hrs070 <harshrajsingh97@gmail.com>
2021-06-25 17:06:31 +0530

    Add navbar for mobile devices

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ScrollReveal from 'scrollreveal';
+
+//select element function
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement(".menu-toggle");
+let body = selectElement("body");
+
+menuToggler.addEventListener("click", () => {
+    body.classList.toggle("open");
+})
