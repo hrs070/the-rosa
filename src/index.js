3f7cb697f8ee51a5090978b009b17f37f1481454
hrs070 <harshrajsingh97@gmail.com>
2021-06-30 11:30:44 +0530

    Improve Animation Timing

--- src/index.js
+++ src/index.js
@@ -21,23 +21,23 @@ sr.reveal(".animate-left", {
     origin: "left",
     duration: 1000,
     distance: "25rem",
-    delay: 300
+    delay: 200
 });
 sr.reveal(".animate-right", {
     origin: "right",
     duration: 1000,
     distance: "25rem",
-    delay: 600
+    delay: 200
 });
 sr.reveal(".animate-top", {
     origin: "top",
     duration: 1000,
     distance: "25rem",
-    delay: 600
+    delay: 200
 });
 sr.reveal(".animate-bottom", {
     origin: "bottom",
     duration: 1000,
     distance: "25rem",
-    delay: 600
+    delay: 200
 })
